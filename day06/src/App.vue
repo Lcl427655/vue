b79464920488f5b3d24cb10fd00f645baf32756d
lcl <26427655@qq.com>
2018-12-11 22:09:43 +0800

    test

---
+++ day06/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Hello Word</h1>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {}
+        },
+        methods:{
+
+        },
+        computed:{
+
+        },
+        components:{
+
+        }
+    }
+</script>
+<!--scoped样式的作用域当前文件-->
+<style scoped>
+
+</style>
--- day06/src/index.js
+++ day06/src/index.js
@@ -21,8 +21,28 @@ import './index.css';//引入css
 import './style.less';//引入less
 
 let img = new Image();
-img.src='./1.png';//webpack会认为他是个字符串，打包之后图片没有，所以使用import或者一个线上路径
+img.src='./1.jpg';//webpack会认为他是个字符串，打包之后图片没有，所以使用import或者一个线上路径
 //可以使用下边的方式
-import page from './1.png';
+import page from './1.jpg';
 img.src=page;
 document.body.appendChild(img);
+
+//导入Vue
+import Vue from 'vue';
+//可以使用下边的方式引入compiler
+//import Vue from 'vue/dist/vue.js';
+//这样引用的是vue.runtime.common.js，而不是html中嵌入的vue.js
+//vue的组成：compiler(编译模板用的，没有的话template不能编译) + runtime
+
+//vue-loader的作用就是将.vue变为模块
+import App from './App.vue';
+console.log(App);
+
+new Vue({
+    //render函数的作用是将虚拟的dom渲染成真实的dom
+    //createElement返回的是虚拟的dom
+    /*render:function (createElement) {
+        return createElement('h1','Hello');
+    }*/
+    render:(e) => e(App)
+}).$mount('#app');
